8293208f7cf2b19581d46029df620761ed03df68
Shaun Scholtz <armysarge.ss@gmail.com>
2024-11-27 16:52:38 +0200

    Added jquery-ui files, made windows draggable and resizable

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,14 @@
+// Document ready function
 $.when( $.ready ).then(function() {
-    console.log("Document ready");
+
+    // Focus on window when clicked
     $(".mythicForgeWindow").on("click tap", function(evt) {
         if ($(this).hasClass("focused")) return;
         $(".mythicForgeWindow").removeClass("focused");
         $(this).css("z-index", mythicForgeWindowIndex()).addClass("focused");
     });
 
+    // Open and close the database window
     $(".showDatabase").on("click tap", function(evt) {
         if ($(".databaseWindow").css("display") == "block") {
             $(".databaseWindow").css("display", "none");
@@ -14,13 +17,21 @@ $.when( $.ready ).then(function() {
         $(".databaseWindow").css({
             "display": "block",
             "z-index": mythicForgeWindowIndex(),
-            "top": "50%",
-            "left": "50%",
-            "transform": "translate(-50%, -50%)"
         });
+        centerDivToScreen(".databaseWindow");
+    });
+
+    //Make windows draggable
+    $(".databaseWindow").draggable({containment: "parent"});
+    $( ".databaseWindow" ).resizable({
+        maxHeight: 600,
+        maxWidth: 800,
+        minHeight: 150,
+        minWidth: 200
     });
 });
 
+// Function to get the highest z-index of all mythicForgeWindows
 function mythicForgeWindowIndex(){
     var highestZ = 0;
     $(".mythicForgeWindow").each(function() {
@@ -29,4 +40,16 @@ function mythicForgeWindowIndex(){
             highestZ = currentZ;
     });
     return highestZ+1;
+}
+
+// Function to center a div to the screen
+function centerDivToScreen(div){
+    var divWidth = $(div).width();
+    var divHeight = $(div).height();
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    $(div).css({
+        "left": windowWidth/2 - divWidth/2,
+        "top": windowHeight/2 - divHeight/2
+    });
 }
