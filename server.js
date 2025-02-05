0b8176c2e49062da9ba169f1164a7f4bbc160642
Shaun Scholtz <armysarge.ss@gmail.com>
2025-02-05 09:04:48 +0200

    Updated import path for settingsManager in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const https = require('https');
 const StreamZip = require('node-stream-zip');
 const AdmZip = require('adm-zip');
 const { rimraf } = require('rimraf');
-const settingsManager = require('./utils/settingsManager');
+const settingsManager = require('./settingsManager');
 
 const DEBUG_MODE = process.env.DEBUG_MODE === 'true' || false;
 
